1aa81058ec4b88b446495ca9493eb46e892eab1c
DuyPN <duy.phan@vtl-vtl.com>
2023-01-13 18:00:06 +0700

    feat: update code

--- static/chat.js
+++ static/chat.js
@@ -71,7 +71,7 @@
 // })
 
 
-const socket = io("http://localhost:3000", { transports : ['websocket'] })
+const socket = io("http://localhost:3000")
 
 const message = document.getElementById('message');
 const messages = document.getElementById('messages');
